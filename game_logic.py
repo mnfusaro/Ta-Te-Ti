b51db592b638906e16609fde5a8b9f92fdc49f75
Nicolas Fusaro <nicolasfusaro96@gmail.com>
2019-09-12 00:08:44 -0300

    initial commit with basic structure ideas

---
+++ game_logic.py
@@ -0,0 +1,25 @@
+
+BOARD_SIZE = 3
+
+
+class Game():
+    def __init__(self):
+        self.board = Board()
+        pass
+
+
+class Board():
+    def __init__(self):
+        self.board = []
+
+    def get_board(self):
+        pass
+
+    def generate_board(self):
+        pass
+
+    def set_mark(self, pos):
+        pass
+
+
+
---
+++ user_interface.py
@@ -0,0 +1,15 @@
+from game_logic import Game
+
+
+def draw_board(board):
+    pass
+
+
+def main():
+    print("Bienvenido al Ta-Te-Ti!\n")
+    juego = Game()
+
+    while True:
+        pass
+
+main()
