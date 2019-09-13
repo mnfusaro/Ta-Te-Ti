c12a74fe695e9eba5fcce37fc5ddee2bb80d8066
Nicolas <nicolasfusaro96@gmail.com>
2019-09-13 01:28:39 -0300

    function to allow user play and some getters were implemented

--- game_logic.py
+++ game_logic.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 _TOKENS = ["X", "O"]
 _BOARD = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
@@ -9,26 +10,49 @@ class Game:
     def __init__(self, user_name):
         self.board = Board()
         self._user = (user_name, random.choice(_TOKENS))
-        self.ia_token = "X" if self.get_user_token() == "O" else "O"
-        self.current_player = self.get_user_name() if random.choice(_TOKENS) == self.get_user_token() else "ia"
+        self._ia_token = "X" if self.get_user_token() == "O" else "O"
+        self.current_player = self.get_user_name() if random.choice(_TOKENS) == self.get_user_token() else "IA"
 
     def make_user_play(self):
-        pass
+        valid_pos = False
+        while not valid_pos:
+            pos = input("What position do you want to mark?(e.g. '1C or B3') ").upper()
+
+            valid_pos = self.__check_valid_pos(pos)
+            if valid_pos:
+                self.board.set_mark(valid_pos, self.get_user_token())
+                break
 
     def make_ia_play(self):
         pass
 
     def change_player_turn(self):
-        pass
+        self.current_player = self.get_user_token() \
+            if self.current_player == self.get_ia_token() else self.get_ia_token()
 
     def check_end_of_game(self):
         pass
 
     def get_user_name(self):
-        pass
+        return self._user[0]
 
     def get_user_token(self):
-        pass
+        return self._user[1]
+
+    def get_ia_token(self):
+        return self._ia_token
+
+    @staticmethod
+    def __check_valid_pos(pos):
+        regex = r'([A-Z][1-3])|([1-3][A-Z])'
+        if len(pos) == 2 and re.match(regex, pos):
+            base_num = ord("A")
+            if pos[0].isdigit():
+                return int(pos[0]), (ord(pos[1]) - base_num)
+            return int(pos[1]), (ord(pos[0]) - base_num)
+
+        else:
+            return False
 
 
 class Board:
